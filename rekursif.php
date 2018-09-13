5abfbd4cba0b669326e9f74765914229f7d97e05
KamalRamadhanN <42663020+KamalRamadhanN@users.noreply.github.com>
2018-09-13 08:45:36 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,42 @@
 <?php
+echo "<br> Deret rekursif<br>";
+$batas = 5;
+function rekursif($a = 1){
+	global $batas;
+  echo "*";
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
+  $a++;
+
+  if($a<= $batas){
+	rekursif($a);
+	}
+		else{
+			echo "<br/>";
+			$batas--;
+			if ($batas!=0) {
+				rekursif();
+			}
+	}
 }
-rekursif(5);
-?>
+
+rekursif();
+echo "Deret Pola rekursif";
+echo "<br> ";
+$garis = 1;
+function rekursiff($b = 1){
+	global $garis;
+  echo "$b";
+  $b++;
+  if($b<= $garis){
+	rekursiff($b);
+	}
+		else{
+			echo "<br>";
+			$garis++;
+			if ($garis<=5) {
+				rekursiff();
+			}
+	}
+}
+rekursiff();
+?
